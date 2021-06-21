e328b48fd419d396932234bd14007c2d9c3b6599
David Salazar <davidjosesalazarmora@gmail.com>
2021-06-21 18:17:33 -0500

    allowed host otra vez 4

--- urls/settings.py
+++ urls/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,5 +158,7 @@ LOGGING = {
 }
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
